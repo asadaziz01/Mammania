dd97d34d6e68b79bb31341467483025534f0b9cd
Asad Aziz <asadrocks50@gmail.com>
2024-06-03 20:54:59 +0530

    landing Page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css';
+import brownieImage from '../../images/first.jpg';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="left">
+        <h1 className="title">Mammania</h1>
+        <p className="subtitle">BROWNIES SO GOOD, THEY'LL DRIVE</p>
+        <p className="subtitle">YOU MAMMA MIA!</p>
+        <button className="shop-button">Shop Online</button>
+      </div>
+      <div className="right">
+        <img src={brownieImage} alt="Brownie" className="brownie-image" />
+      </div>
+    </div>
+  )
+}
