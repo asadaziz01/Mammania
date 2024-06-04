a015cdffb72babbaac32169ad888b79e334b7cc2
Asad Aziz <asadrocks50@gmail.com>
2024-06-05 01:33:32 +0530

    Error Page

---
+++ src/components/NotFoundErrorPage/NotFoundErrorPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './NotFoundErrorPage.css'; 
+import { useNavigate } from 'react-router-dom';
+
+const NotFoundErrorPage = () => {
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate('/');
+  };
+  return (
+    <div className="error-container">
+      <h1>Error 404!!</h1>
+      <p>The page you are looking for does not exist.</p>
+      <button onClick={goToHome} className="home-button">Go to Home</button>
+    </div>
+  );
+};
+
+export default NotFoundErrorPage;
